054a7b2e0952a8aff14995928197009e78268f8d
abesselem18275 <abesselem.idani@gmail.com>
2018-10-31 16:08:48 +0100

    hello

--- productAPI/serializers.py
+++ productAPI/serializers.py
@@ -2,24 +2,27 @@ from rest_framework import serializers
 from productAPI.models import Product, Famille, Laboratoire
 
 
+class ProductSerializer(serializers.ModelSerializer):  
+
+    
+    class Meta:
+        model = Product
+        fields = '__all__'
+        depth = 1
+
 
 class FamilleSerializer(serializers.ModelSerializer):
 
-    products = serializers.StringRelatedField(many=True)
+    products = ProductSerializer(many=True,read_only=True)
     class Meta:
         model = Famille
         fields = '__all__'
 
 class LaboratoireSerializer(serializers.ModelSerializer):
      
-    products = serializers.StringRelatedField(many=True)
+    products = ProductSerializer(many=True,read_only=True)
 
     class Meta:
         model = Laboratoire
         fields = '__all__'
 
-class ProductSerializer(serializers.ModelSerializer):  
-
-    class Meta:
-        model = Product
-        fields = '__all__'
