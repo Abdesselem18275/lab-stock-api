47107aed8f6580bd41347e3056ba9b443221ffd1
abesselem18275 <abesselem.idani@gmail.com>
2018-11-23 20:37:26 +0100

    models constrainst refinement

--- productAPI/models.py
+++ productAPI/models.py
@@ -6,20 +6,19 @@ TRANS_TYPE = (
     ('IN', 'ENTREE'),
     ('OUT', 'SORTIE'))
 
-
 class Famille(models.Model):
-    designation = models.CharField(unique=True,max_length=100, blank=True, default='') 
+    designation = models.CharField(unique=True,max_length=100, blank=False, default='') 
     def __str__(self):
         return self.designation
 
 class Laboratoire(models.Model):
-    designation = models.CharField(unique=True,max_length=100, blank=True, default='') 
+    designation = models.CharField(unique=True,max_length=100, blank=False, default='') 
     def __str__(self):
         return self.designation
 
 class Product(models.Model):
-    designation = models.CharField(unique=True,max_length=100, blank=True, default='')
-    reference = models.CharField(max_length=100, default='')
+    designation = models.CharField(unique=True,max_length=100, blank=False, default='')
+    reference = models.CharField(max_length=100, default='',blank=False)
     creation_date = models.DateField(default=datetime.date.today)
     contenantCoffret = models.IntegerField(default='1', null=True)
     testContenant = models.IntegerField(default='1', null=True)
@@ -27,7 +26,6 @@ class Product(models.Model):
     StockMiniMois = models.IntegerField(default='1', null=True)
     famille = models.ForeignKey(Famille, related_name='products',on_delete=models.SET_NULL, blank=True, null=True)
     laboratoire = models.ForeignKey(Laboratoire,related_name='products', on_delete=models.SET_NULL, blank=True, null=True)
-
     def __str__(self):
         return self.designation
 
@@ -35,10 +33,8 @@ class ProductTrans(models.Model):
     numero_lot = models.IntegerField(unique=True,blank=False)
     produit = models.ForeignKey(Product,related_name='transactions', on_delete=models.SET_NULL, blank=True, null=True)
     quantite = models.IntegerField(default='1',blank=False)
-    modification_date=models.DateTimeField(default=datetime.date.today)
-    peremption_date = models.DateField(default=datetime.date.today)
+    modification_date=models.DateTimeField(default=datetime.date.today,blank=False)
+    peremption_date = models.DateField(default=datetime.date.today,blank=False)
     trans_type = models.CharField(max_length=10,choices=TRANS_TYPE)
     def __str__(self):
         return self.designation
-    
-
