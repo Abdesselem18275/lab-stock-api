a5031020ed4b68e9d24250379109b74049105465
abesselem18275 <abesselem.idani@gmail.com>
2018-12-04 13:32:33 +0100

    Edit model ( Modification and creation date )

--- productAPI/models.py
+++ productAPI/models.py
@@ -8,11 +8,18 @@ TRANS_TYPE = (
 
 class Famille(models.Model):
     designation = models.CharField(unique=True,max_length=100, blank=False, default='') 
+    modification_date=models.DateTimeField(default=datetime.date.today,blank=False)
+    creation_date = models.DateField(default=datetime.date.today)
+
     def __str__(self):
         return self.designation
 
 class Laboratoire(models.Model):
     designation = models.CharField(unique=True,max_length=100, blank=False, default='') 
+    modification_date=models.DateTimeField(default=datetime.date.today,blank=False)
+    creation_date = models.DateField(default=datetime.date.today)
+
+
     def __str__(self):
         return self.designation
 
@@ -20,6 +27,7 @@ class Product(models.Model):
     designation = models.CharField(unique=True,max_length=100, blank=False, default='')
     reference = models.CharField(max_length=100, default='',blank=False)
     creation_date = models.DateField(default=datetime.date.today)
+    modification_date=models.DateTimeField(default=datetime.date.today,blank=False)
     contenantCoffret = models.IntegerField(default='1', null=True)
     testContenant = models.IntegerField(default='1', null=True)
     cmm = models.IntegerField(default='1', null=True)
@@ -34,6 +42,7 @@ class ProductTrans(models.Model):
     produit = models.ForeignKey(Product,related_name='transactions', on_delete=models.SET_NULL, blank=True, null=True)
     quantite = models.IntegerField(default='1',blank=False)
     modification_date=models.DateTimeField(default=datetime.date.today,blank=False)
+    creation_date = models.DateField(default=datetime.date.today)
     peremption_date = models.DateField(default=datetime.date.today,blank=False)
     trans_type = models.CharField(max_length=10,choices=TRANS_TYPE)
     def __str__(self):
